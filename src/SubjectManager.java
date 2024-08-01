a0c2907b97bd8f384c0e6422a0fa4b786eae9a31
JH1Yoon <wlgus4589@naver.com>
2024-08-01 18:50:34 +0900

    SubjectManager 클래스로 이름 변경

--- src/SubjectManager.java
+++ src/SubjectManager.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class SubjectImpl {
+public class SubjectManager {
 
     private static List<Subject> subjectList = new ArrayList<>();
 
