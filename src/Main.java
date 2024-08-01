86fd23c51ecd1780b2750460159bca910fc235d6
Aakaive <wonseok031@gmail.com>
2024-08-01 13:29:29 +0900

    create Score class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    public Score score;
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
--- src/Score.java
+++ src/Score.java
@@ -1,3 +1,47 @@
 public class Score {
+    private int subNo;
+    private int stdNo;
+    private int term;
+    private int score;
+    private char grade;
+
+    public Score(int subNo, int stdNo, int term, int score, char grade) {
+        this.subNo = subNo;
+        this.stdNo = stdNo;
+        this.term = term;
+        this.score = score;
+        this.grade = grade;
+    }
+
+    public int getSubNo() {
+        return subNo;
+    }
+    public void setSubNo(int subNo) {
+        this.subNo = subNo;
+    }
+    public int getStdNo() {
+        return stdNo;
+    }
+    public void setStdNo(int stdNo) {
+        this.stdNo = stdNo;
+    }
+    public int getTerm() {
+        return term;
+    }
+    public void setTerm(int term) {
+        this.term = term;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public char getGrade() {
+        return grade;
+    }
+    public void setGrade(char grade) {
+        this.grade = grade;
+    }
 
 }
