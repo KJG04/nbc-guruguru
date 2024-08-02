fef900f847aa81a35e269c45cd403687a6486f5b
Aakaive <wonseok031@gmail.com>
2024-08-02 10:54:40 +0900

    Score 인스턴스 중복 검사 - contains의 equals 오버라이드. - Score의 필드 세개의 값으로 해쉬

--- src/Score.java
+++ src/Score.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Score {
     private int subNo;
     private int stdNo;
@@ -5,43 +7,75 @@ public class Score {
     private int score;
     private char grade;
 
-    public Score(int subNo, int stdNo, int term, int score, char grade) {
+    public Score(int subNo, int stdNo, int term, int score) {
         this.subNo = subNo;
         this.stdNo = stdNo;
         this.term = term;
         this.score = score;
-        this.grade = grade;
+        setGrade(score);
     }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Score sc)) return false;
+        return sc.getSubNo() == this.subNo && sc.getStdNo() == this.stdNo && sc.getTerm() == this.term;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subNo, stdNo, term);
+    }
+
 
     public int getSubNo() {
         return subNo;
     }
-    public void setSubNo(int subNo) {
-        this.subNo = subNo;
-    }
     public int getStdNo() {
         return stdNo;
-    }
-    public void setStdNo(int stdNo) {
-        this.stdNo = stdNo;
     }
     public int getTerm() {
         return term;
     }
-    public void setTerm(int term) {
-        this.term = term;
-    }
     public int getScore() {
         return score;
-    }
-    public void setScore(int score) {
-        this.score = score;
     }
     public char getGrade() {
         return grade;
     }
-    public void setGrade(char grade) {
-        this.grade = grade;
+
+    public void setSubNo(int subNo) {
+        this.subNo = subNo;
     }
 
+    public void setStdNo(int stdNo) {
+        this.stdNo = stdNo;
+    }
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+        setGrade(score);
+    }
+
+    public void setGrade(int score) {
+        if(score >= 95 && score <= 100) {
+            this.grade = 'A';
+        }
+        else if(score >= 90 && score <= 94) {
+            this.grade = 'B';
+        }
+        else if(score >= 80 && score <= 89) {
+            this.grade = 'C';
+        }
+        else if(score >= 70 && score <= 79) {
+            this.grade = 'D';
+        }
+        else if(score >= 60 && score <= 69) {
+            this.grade = 'F';
+        }
+    }
 }
